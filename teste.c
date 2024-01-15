b487f2bef15925aa9b29a8d99b303cd92ac00348
Marcos <marcos.mb.mb@outlook.com>
2024-01-14 21:54:11 -0300

    p2

--- teste.c
+++ teste.c
@@ -2,21 +2,33 @@
 #include <string.h>
 #include <windows.h>
 
+void tratarString(char palavra[]) {
+  unsigned int i, k = 0;
+  char comAcentos[] = "ÄÅÁÂÀÃäáâàãÉÊËÈéêëèÍÎÏÌíîïìÖÓÔÒÕöóôòõÜÚÛüúûùÇç";
+  char semAcentos[] = "AAAAAAaaaaaEEEEeeeeIIIIiiiiOOOOOoooooUUUuuuuCc";
+  //Removendo acentos
+  for (k = 0; k < strlen(palavra); k++) {
+
+  for (i = 0; i < strlen(comAcentos); i++) {
+  if (palavra[k] == comAcentos[i]) {
+  palavra[k] = semAcentos[i];
+  printf("%c", comAcentos[i]);
+  printf("\n");
+  }
+  }
+  }
+    }
+
 int main(){
 
     SetConsoleOutputCP(CP_UTF8);
 
-    char string_one[] = "marcos";
+    char string_one[] = "Você é legal";
 
-    char string_two[] = "marcos";
+    tratarString(string_one);
 
-    if(strcmp(string_one, string_two) == 0){
-        printf("São iguais");
-    }
-    else{
-        printf("São diferentes");
-    }
-
+    printf("%s\n", string_one);
+    
 
     return 0;
 }
